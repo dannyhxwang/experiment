09fa3fc1058bcd4df499bef4936631f9a7ca8b31
dannyhxwang <dannyhxwang@gmail.com>
2014-10-31 17:48:41 +0800

    test

---
+++ src/main/java/com/bigdata/test/hadoop/Test1.java
@@ -0,0 +1,10 @@
+package com.bigdata.test.hadoop;
+
+/**
+ * Created by wanghaixing on 14-10-31.
+ */
+public class Test1 {
+    public void main(String[] args) {
+        System.out.println("hello hadoop.");
+    }
+}
