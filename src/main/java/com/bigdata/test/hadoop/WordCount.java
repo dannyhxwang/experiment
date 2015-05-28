700a39a8ceb6e7086329b54f6334e67522838717
dannyhxwang <dannyhxwang@gmail.com>
2015-05-28 22:59:15 +0800

    test

--- src/main/java/com/bigdata/test/hadoop/WordCount.java
+++ src/main/java/com/bigdata/test/hadoop/WordCount.java
@@ -36,13 +36,13 @@ public class WordCount {
         //1.1 输入目录在哪里
         FileInputFormat.setInputPaths(job, INPUT_PATH);
         //指定对输入数据进行格式化处理的类
-//		job.setInputFormatClass(TextInputFormat.class);
+		job.setInputFormatClass(TextInputFormat.class);
 
         //1.2 指定自义的Mapper类
-        job.setMapperClass(MyMapper.class);
+        job.setMapperClass(WordCountMapper.class);
         //指定map输出的<k,v>类型。如果<k3,v3>的类型与<k2,v2>的类型一致，那么可以省略
-//		job.setMapOutputKeyClass(Text.class);
-//		job.setMapOutputValueClass(LongWritable.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(LongWritable.class);
 
         //1.3 分区
 //		job.setPartitionerClass(HashPartitioner.class);
@@ -53,14 +53,14 @@ public class WordCount {
         //1.5 TODO （可选）归约
 
         //2.2 指定自定义的Reduce类
-        job.setReducerClass(MyReducer.class);
+        job.setReducerClass(WordCountReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(LongWritable.class);
 
         //2.3 指定输出 的路径
         FileOutputFormat.setOutputPath(job, new Path(OUTPUT_PATH));
         //指定输出的格式化类
-//		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
 
         //把作业提交给JobTracker运行
         job.waitForCompletion(true);
@@ -72,7 +72,7 @@ public class WordCount {
      * KEYOUT	即k2		表示每一行中的每个单词
      * VALUEOUT	即v2		表示每一行中的每个单词的出现次数，固定值1
      */
-    static class MyMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+    class WordCountMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
         protected void map(LongWritable k1, Text v1, Context ctx) throws IOException ,InterruptedException {
             String[] splited = v1.toString().split("\t");
             for (String word : splited) {
@@ -87,7 +87,7 @@ public class WordCount {
      * KEYOUT	即k3		表示整个文件中的不同单词
      * VALUEOUT	即v3		表示整个文件中不同单词出现的总次数
      */
-    static class MyReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
+    class WordCountReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
         protected void reduce(Text k2, Iterable<LongWritable> values, Context ctx) throws IOException ,InterruptedException {
             long sum = 0L;
             for (LongWritable value : values) {
